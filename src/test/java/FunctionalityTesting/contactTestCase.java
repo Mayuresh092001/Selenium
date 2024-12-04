d5e55b863f9c278d63f14d103704fd6fb4081064
Mayuresh092001 <mayurvankudre09@gmail.com>
2024-12-04 11:22:08 +0530

    Selenium

--- src/test/java/FunctionalityTesting/contactTestCase.java
+++ src/test/java/FunctionalityTesting/contactTestCase.java
@@ -1,5 +1,23 @@
 package FunctionalityTesting;
 
+import org.testng.annotations.Test;
+
+import com.crm.BaseClass.BaseClass;
+import com.crm.pom.contactsRepo;
+
 public class contactTestCase {
+	
+	
+	public class contactTestcase extends BaseClass{
+		@Test
+		public void contact_page(){
+			String actual_url= driver.getCurrentUrl();
+			contactsRepo cr = new contactsRepo();
+			
+			
+			
+		}
+		
+	}
 
 }
